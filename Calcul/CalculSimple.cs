e977a9f0a8f91197b5089429c5c25a908361b948
Straulqued <55499702+Straulqued@users.noreply.github.com>
2021-06-13 15:48:33 +0200

    Add files via upload

---
+++ Calcul/CalculSimple.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+using System;
+
+namespace Calculatrice.Calcul
+{
+    public class CalculSimple
+    {
+        public static int Calcul(string operation, int a, int b)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return a + b;
+                    break;
+                
+                case "-":
+                    return a - b;
+                    break;
+                
+                case "*":
+                    return a * b;
+                    break;
+                
+                case "/":
+                    return a / b;
+                    break;
+                default:
+                    return 0000000;
+                    break;
+            }
+        }
+    }
+}
