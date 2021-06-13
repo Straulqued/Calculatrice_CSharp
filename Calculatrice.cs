bc94cf30e2d98144b1a55ee861440c0868f79d23
Straulqued <55499702+Straulqued@users.noreply.github.com>
2021-06-13 15:46:38 +0200

    Add files via upload

---
+++ Calculatrice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Calculatrice.Calcul;
+
+namespace Calculatrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Bienvenue-----");
+            Console.WriteLine("Quel type de calcul voulez vous faire?");
+            string t = Console.ReadLine();
+            
+            switch (t)
+            {
+                case "a":
+                    Console.WriteLine("Test");
+                    break;
+                
+                case "b":
+                    Console.WriteLine("Quel type d'oppération voulez vous faire? ");
+                    string a = Console.ReadLine();
+                    
+                    Console.WriteLine("Quel est le premier nombre du calcul");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    
+                    Console.WriteLine("Quel est le second nombre du calcul");
+                    int c = Convert.ToInt32(Console.ReadLine());
+
+                    int oe = CalculSimple.Calcul("a", b, c);
+                    Console.WriteLine("Le résultat est : " + oe );
+                    string resultat = b.ToString() + " " + a + " " + c.ToString() + " = " + oe;
+                    
+                    
+                    break;
+                
+            }
+        }
+    }
+}
